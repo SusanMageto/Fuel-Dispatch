6ce0c844447342fb6805b51a2cf9b75154484ee8
Edwin Karimi Mureithi <edumuriithi58@gmail.com>
2022-02-08 09:31:29 +0300

    first commit with components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { SearchComponent } from './components/search/search.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    SearchComponent,
+    ContactComponent,
+    AboutComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
